89615ff9b92af400969a69a01d47cba23de77b64
ederson lopez <ederson12f@gmail.com>
2021-09-19 21:47:39 -0500

    configuration with webpack in production (html , css y images)

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const htmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
+	entry: path.resolve(__dirname, 'src/index.js'),
+	mode: 'production',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
+		filename: 'bundle[contenthash].js',
 		publicPath: '/',
 		clean: true,
 	},
@@ -20,6 +22,31 @@ module.exports = {
 					loader: 'babel-loader',
 				},
 			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: { minimize: true},
+					},
+				],
+			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				type: 'asset',
+				test: /\.(png|jpg|svg)$/i,
+			},
 		],
 	},
+
+	plugins: [
+		new htmlWebPackPlugin({
+			injet: true,
+			template: path.resolve(__dirname, 'public/index.html'),
+			filename: './index.html',
+		}),
+	],
 };
