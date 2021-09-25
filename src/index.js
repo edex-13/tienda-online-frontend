340bfe60a22a2407fcb22a52f06365de16e114e8
ederson lopez <ederson12f@gmail.com>
2021-09-24 21:37:19 -0500

    modal design

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-import './styles/app.scss'
+import './styles/app.scss';
 const a = 1;
 const b = 2;
 console.log(a + b);
-
-window.onpopstate = function(event) {
-  alert("location: " + document.location + ", state: " + JSON.stringify(event.state));
-};
