12a997eecb7028d5757e41d8d0b3fb28ab316368
ederson lopez <ederson12f@gmail.com>
2021-09-25 22:52:30 -0500

    get product data to add to cart and save it in localStorage

--- src/main/components/cardProduct/CardProduct.js
+++ src/main/components/cardProduct/CardProduct.js
@@ -1,23 +1,24 @@
-const createContainerProductInfo = (title , price) => {
+const createContainerProductInfo = (title, price) => {
 	const $titleProduct = document.createElement('h2');
-	$titleProduct.classList.add('card-product__title');
+	$titleProduct.classList.add('card-product__title' , 'title_product');
 	$titleProduct.textContent = title;
 
 	const $priceProduct = document.createElement('p');
-	$priceProduct.classList.add('card-product__price');
+	$priceProduct.classList.add('card-product__price', 'price_product');
 	$priceProduct.textContent = price;
 
 	return [$titleProduct, $priceProduct];
 };
 
-
 const CardProduct = (product) => {
-	const {title,description, routeImage , price} = product;
+	const {id, title, description, routeImage, price} = product;
 
 	const $cardProduct_container = document.createElement('div');
 	$cardProduct_container.classList.add('card-product');
+	$cardProduct_container.id = id;
 
 	const $imageProduct = document.createElement('img');
+	$imageProduct.classList.add('image_product');
 	$imageProduct.src = routeImage;
 	$imageProduct.alt = title;
 	$imageProduct.title = title;
@@ -29,20 +30,20 @@ const CardProduct = (product) => {
 	$container_ProductInfo.classList.add('container-product-info');
 
 	const $text_descriptionProduct = document.createElement('p');
-	$text_descriptionProduct.classList.add('card-product__description');
+	$text_descriptionProduct.classList.add('card-product__description', 'description_product');
 	$text_descriptionProduct.textContent = description;
 
-  const $btn_AddToCart = document.createElement('button');
-	$btn_AddToCart.classList.add('btn', 'btn-secondary');
+	const $btn_AddToCart = document.createElement('button');
+	$btn_AddToCart.classList.add('btn', 'btn-secondary' , 'btn_addToCart');
 	$btn_AddToCart.title = 'Agregar al carrito';
 	$btn_AddToCart.textContent = 'Agregar al carrito';
 
-  const child = createContainerProductInfo(title , price)
-  $container_ProductInfo.append(...child)
-  $cardProduct_info.append($container_ProductInfo,$text_descriptionProduct,$btn_AddToCart)
-  $cardProduct_container.append($imageProduct,$cardProduct_info)
-  
-  return $cardProduct_container;
+	const child = createContainerProductInfo(title, price);
+	$container_ProductInfo.append(...child);
+	$cardProduct_info.append($container_ProductInfo, $text_descriptionProduct, $btn_AddToCart);
+	$cardProduct_container.append($imageProduct, $cardProduct_info);
+
+	return $cardProduct_container;
 };
 
-export default CardProduct
+export default CardProduct;
