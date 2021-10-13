d245e047cf094dd4f648e40f06c63f02e46be5a3
ederson lopez <ederson12f@gmail.com>
2021-10-12 21:07:34 -0500

    start of  shopping cart

---
+++ src/main/components/templates/headerModal.js
@@ -0,0 +1,9 @@
+const HeaderModal = (title)=>{
+  return `
+    <div class="modal-header">
+      <h2 class="title-modal">${title}</h2>
+      <button class="btn-transition close_btn close-modal"></button>
+    </div>
+  `
+}
+export default HeaderModal;
