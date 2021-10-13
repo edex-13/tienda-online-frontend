2fdc97ad65737597b83348740f35d8c11113bd0b
ederson lopez <ederson12f@gmail.com>
2021-10-13 13:16:35 -0500

    modify the quantity of the products

--- src/main/components/shoppinCart/index.js
+++ src/main/components/shoppinCart/index.js
@@ -1,8 +1,10 @@
 import {openModal,closeModal} from'actions/openModal';
 import updateCarContent from './utils/cartContent';
+import modifyQuantity from 'components/itemProduct/utils/modifyQuantity';
 
 const $btn_activeModal = document.querySelector('#openModal');
 const $btn_closeModal = document.querySelector('.close-modal');
+const $modal_body = document.querySelector('.modal-body')
 
 $btn_activeModal.addEventListener('click', () => {
 	openModal()
@@ -12,3 +14,7 @@ $btn_activeModal.addEventListener('click', () => {
 $btn_closeModal.addEventListener('click', () => {
   closeModal()
 });
+
+ $modal_body.addEventListener('click', (event)=>{
+   modifyQuantity(event)
+ })
