844086256d26912c6e33d0d707c5a7ee5e230fc9
ederson lopez <ederson12f@gmail.com>
2021-10-13 22:05:53 -0500

    be able to empty the shopping cart

--- src/main/components/shoppinCart/utils/cartContent.js
+++ src/main/components/shoppinCart/utils/cartContent.js
@@ -3,14 +3,19 @@ import templateNoProducts from 'components/templates/noProducts';
 import nodeProduts from 'components/itemProduct/utils/getProducts';
 
 const $modal_body = document.querySelector('.modal-body')
+const $btn_empty = document.querySelector('#btn_empty')
 
 const cartContent = () => {
   const areThereProducts = JSON.parse(getCollection('PRODUCTS'))
   $modal_body.innerHTML = ""
   if (areThereProducts) {
     $modal_body.append(...nodeProduts())
+    $modal_body.classList.remove('noProducts')
+    $btn_empty.classList.remove('hidden')
   } else {
     $modal_body.innerHTML = templateNoProducts()
+    $modal_body.classList.add('noProducts')
+    $btn_empty.classList.add('hidden')
   }
 }
 
