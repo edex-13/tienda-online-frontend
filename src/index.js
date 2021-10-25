f08450b72e3a4d9353d98866dd8db82a2bdcb8cf
ederson lopez <ederson12f@gmail.com>
2021-10-24 22:42:36 -0500

    search Inputs

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import 'components/cardProduct/utils/addToCart';
 
 import 'components/shoppinCart/index';
 
-import routres from 'routes/index';
+import routes from 'routes/index';
+import paramsQuery from 'routes/paramsQuery';
 
 
-const container = document.querySelector('#cards-products');
-
+const $container = document.querySelector('#cards-products');
+const $searchInputs = [...document.querySelectorAll('.search_btn')]
 
 window.addEventListener('load', async(event) => {
-	const nodeProducts =await routres(location)
-	container.append(...nodeProducts)
+	const nodeProducts =await routes(location)
+	$container.append(...nodeProducts)
 });
+
+$searchInputs.forEach(searchInput =>{
+	searchInput.addEventListener('click', (e)=>{
+		const $container = e.path[2]
+		const param =  $container.querySelector('input').value
+		paramsQuery(param)
+	})
+})
